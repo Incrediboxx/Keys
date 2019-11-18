f03ceab109811eda055f59327e22a040c20d6ff3
Incrediboxx <nbykov@diasoft.ru>
2019-11-18 17:52:57 +0300

    123

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,10 +10,12 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Fraction fraction = new Fraction("8","16");
-            fraction.Print();
-            fraction.Normalizer();
-            fraction.Print();
+            Fraction f1 = new Fraction("1","2");
+            Fraction f2 = new Fraction("2", "16");
+            Fraction f3 = f1 + f2;
+            f3.Print();
+            f3.Normalizer();
+            f3.Print();
 
 
             Console.ReadKey();
